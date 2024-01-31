dcc5d68e3342079ee9a0115529bd21a456d78f7e
Matheus Mello <matheusgmello1@gmail.com>
2024-01-31 14:28:47 -0300

    Feat: Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ export function App() {
 
   return (
     <div>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post
+        author="Matheus Mello"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
