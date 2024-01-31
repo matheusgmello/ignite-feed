82369a2fed4ac5d195cbb5227c93701282553c8c
Matheus Mello <matheusgmello1@gmail.com>
2024-01-31 14:37:00 -0300

    Feat: CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Post } from "./Post";
+import { Header } from './components/Header';
+import { Post } from './Post'
+
+import './styles.css';
 
 export function App() {
-
   return (
     <div>
+      <Header />
       <Post
         author="Matheus Mello"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
