5ff0fefd66bc9015587d30ba1777f54655905119
Matheus Mello <matheusgmello1@gmail.com>
2024-02-01 18:37:15 -0300

    Feat: Componente: Comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -25,6 +26,7 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
@@ -36,6 +38,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
