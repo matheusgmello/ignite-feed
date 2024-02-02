d019d2dc6288c28dbeba14aa4c806c4a293188b6
Matheus Mello <matheusgmello1@gmail.com>
2024-02-02 11:29:02 -0300

    Feat:  Inserindo comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,16 +3,16 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/matsz22.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/GuilhermeHS2.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Matheus Santos</strong>
+              <strong>Guilherme Henrique</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
@@ -21,7 +21,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Ficou show irmão!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
